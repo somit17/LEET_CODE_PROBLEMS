dd538a078ebdb826a8fcbfabd3dc5f25343287cb
Somit Meshram <somitmeshram3256@gmail.com>
2023-10-03 18:46:06 +0530

    Time: 176 ms (75.56%), Space: 50.4 MB (81.95%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int max = int.MinValue; // maximum sum
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            if (sum > max)
+            {
+                max = sum;
+            }
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
