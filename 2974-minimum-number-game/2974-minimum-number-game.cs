c1aa4f77df298241a6fb5f8f80ed039d45ce0ff4
Somit Meshram <somitmeshram3256@gmail.com>
2024-01-16 23:49:15 +0530

    Time: 119 ms (28.47%), Space: 48.6 MB (42.34%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cs
+++ 2974-minimum-number-game/2974-minimum-number-game.cs
@@ -2,21 +2,13 @@ public class Solution {
     public int[] NumberGame(int[] nums) {
         List<int> numsReturn = new();
         System.Array.Sort(nums);
-        List<int> aliceStack = new();
-        List<int> bobStack = new();
-        for (int i = 0; i < nums.Length; i+=2)
-        {
-            aliceStack.Add(nums[i]);
-        }
-        for (int i = 1; i < nums.Length; i+=2)
-        {
-            bobStack.Add(nums[i]);
-        }
-
-        for (int i = 0; i < nums.Length/2; i++)
-        {
-            numsReturn.Add(bobStack[i]);
-            numsReturn.Add(aliceStack[i]);
+        int lp = 0, rp = 1;
+        while (lp < nums.Length-1 || rp < nums.Length-1)
+        { 
+            numsReturn.Add(nums[rp]);
+            rp += 2;
+            numsReturn.Add(nums[lp]);
+            lp += 2;
         }
         return numsReturn.ToArray();
     }
