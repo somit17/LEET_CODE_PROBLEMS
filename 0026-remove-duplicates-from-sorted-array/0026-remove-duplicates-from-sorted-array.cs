54402afa691b4ac770e30e72a7ee73ada971ad39
Somit Meshram <somitmeshram3256@gmail.com>
2024-03-07 22:55:14 +0530

    Time: 111 ms (91.59%), Space: 50.2 MB (17.52%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        
+        
+        int left = 1;
+        for(int right=1;right<nums.Length;right++){
+            if(nums[right]!=nums[right-1]){
+                nums[left] = nums[right];
+                left++;
+                
+            }
+        }
+        return left;
+    }
+}
