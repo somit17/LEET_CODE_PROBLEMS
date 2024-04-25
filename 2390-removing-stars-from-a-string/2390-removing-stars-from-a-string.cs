24e4b906d3aa1ce2c3e93946b5723bfbb63bd694
Somit Meshram <somitmeshram3256@gmail.com>
2024-04-25 11:56:45 +0530

    Time: 75 ms (71.37%), Space: 46.4 MB (82.69%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cs
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public string RemoveStars(string s) {
-       Stack<char> stack = new Stack<char>();
-        foreach (var eachChar in s)
+      Stack<char> stack = new();
+        foreach (char eachChar in s)
         {
             if (eachChar.Equals('*'))
             {
@@ -13,18 +13,6 @@ public class Solution {
             }
         }
 
-        List<char> finalOriginalString = new();
-        while (stack.Count>0)
-        {
-            finalOriginalString.Add(stack.Pop());
-        }
-
-        StringBuilder returnString = new StringBuilder();
-        for (int i = finalOriginalString.Count-1; i >=0 ; i--)
-        {
-            returnString.Append(finalOriginalString[i]);
-        }
-         
-        return returnString.ToString(); 
+        return string.Join("", stack.Reverse());  
     }
 }
