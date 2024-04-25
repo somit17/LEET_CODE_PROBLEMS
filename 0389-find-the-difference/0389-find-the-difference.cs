7a2f2eecc6c46f5cdf803d8ee21155254627060a
Somit Meshram <somitmeshram3256@gmail.com>
2024-04-25 22:20:05 +0530

    Time: 62 ms (73.75%), Space: 41.8 MB (26.08%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -1,44 +1,15 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
-      Dictionary<char, int> mapS = new(); //map char:int
-        Dictionary<char, int> mapT = new();
-        foreach (var eachChar in s)
+      int s_Sum = 0, t_Sum = 0;
+        foreach (var item in s)
         {
-            if (mapS.ContainsKey(eachChar))
-            {
-                mapS.TryGetValue(eachChar, out int val);
-                mapS[eachChar] = val + 1;
-            }
-            else
-            {
-                mapS.Add(eachChar, 1);
-            }
+            s_Sum += item;
+        }
+        foreach (var item in t)
+        {
+            t_Sum += item;
         }
 
-        foreach (var eachChar in t)
-        {
-            if (mapT.ContainsKey(eachChar))
-            {
-                mapT.TryGetValue(eachChar, out int val);
-                mapT[eachChar] = val + 1;
-            }
-            else
-            {
-                mapT.Add(eachChar, 1);
-            }
-        }
-
-        foreach (var chars in t)
-        {
-            if (mapS.ContainsKey(chars))
-            {
-                if (mapT[chars] != mapS[chars])
-                    return chars;
-            }
-            else
-                return chars;
-        }
-
-        return ' ';
+        return (char) (t_Sum - s_Sum);
     }
 }
