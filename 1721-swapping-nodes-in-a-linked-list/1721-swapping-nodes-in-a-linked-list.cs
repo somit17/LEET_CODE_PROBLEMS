5c19179076b74373167d93ca47a68147e945337c
Somit Meshram <somitmeshram3256@gmail.com>
2024-04-26 19:56:07 +0530

    Time: 360 ms (60.68%), Space: 80.4 MB (77.78%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cs
@@ -11,20 +11,28 @@
  */
 public class Solution {
     public ListNode SwapNodes(ListNode head, int k) {
-         int pos = 0, length = 0;
-        ListNode current = head,left=current,right=head;
-        for (int i = 0; i < k-1; i++)
+     int Length = 0,L=k,R=0;
+        ListNode current = head, Node1 = head, Node2 = head;
+         while (current!= null)
         {
             current = current.next;
-        }
-        left = current;
-        while (current.next is not null)
-        {
-            current = current.next;
-            right = right.next;
+            Length += 1;
         }
 
-        (left.val, right.val) = (right.val, left.val);
-        return head;
+        R = Length - k + 1;
+        while (L > 1)
+        {
+            Node1 = Node1.next;
+            L--;
+        }
+
+        while (R > 1)
+        {
+            Node2 = Node2.next;
+            R--;
+        }
+
+        (Node1.val, Node2.val) = (Node2.val, Node1.val);
+        return head;   
     }
 }
