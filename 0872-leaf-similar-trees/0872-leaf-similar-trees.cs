c009964e7b9a0b9406855f54c931accacbb74cf9
Somit Meshram <somitmeshram3256@gmail.com>
2024-05-07 20:39:29 +0530

    Time: 75 ms (31.32%), Space: 43 MB (37.36%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cs
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cs
@@ -13,37 +13,17 @@
  */
 public class Solution {
     public bool LeafSimilar(TreeNode root1, TreeNode root2) {
-        return LeafNodesFromTree(root1).SequenceEqual(LeafNodesFromTree(root2));
+       List<int> leafNodes1 = new(), leafNodes2 = new();
+        DFS(root1,leafNodes1); 
+        DFS(root2,leafNodes2);
+        return leafNodes1.SequenceEqual(leafNodes2);
     }
-    
-public List<int> LeafNodesFromTree(TreeNode root)
+    public void DFS(TreeNode root, List<int> leafNodes)
     {
-        if (root == null)
-        {
-            return new List<int>();
-        }
-
-        Stack<TreeNode> stack = new();
-        List<int> leafNodes = new();
-        stack.Push(root);
-
-        while (stack.Count > 0)
-        { var node = stack.Pop();
-            if(node.left==null && node.right==null)
-                leafNodes.Add(node.val);
-               
-            if (node.right != null)
-            {
-                stack.Push(node.right);
-            }
-
-            if (node.left != null)
-            {
-                stack.Push(node.left);
-            }
-               
-        }
-
-        return leafNodes;
+        if (root == null) return;
+        if(root.left==null && root.right==null)
+            leafNodes.Add(root.val);
+        DFS(root.left,leafNodes);
+        DFS(root.right,leafNodes);
     }
 }
