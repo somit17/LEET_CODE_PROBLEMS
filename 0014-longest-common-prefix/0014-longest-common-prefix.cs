f5a5eae848d9fe45005e5a345e3c0d450d974dd3
Somit Meshram <somitmeshram3256@gmail.com>
2024-05-15 14:59:19 +0530

    Time: 62 ms (86.26%), Space: 42.2 MB (76.16%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,22 +1,45 @@
 public class Solution {
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> Children = new();
+        public bool IsLast = false;
+    }
+    private TrieNode root;
+    public Solution()
+    {
+        root = new();
+    }
     public string LongestCommonPrefix(string[] strs) {
-          System.Array.Sort(strs);
-        string first = strs[0];
-        string last = strs[strs.Length - 1];
-
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < Math.Min(first.Length,last.Length); i++)
+        foreach (var s in strs)
+            InsertWords(s);
+        return StartsWith(root,"");
+    }
+    void InsertWords(string s)
+    {
+        var current = root;
+        foreach (char ch in s)
         {
-            if (first[i] != last[i])
-            {
-                return ans.ToString();
-            }
-            else
-            {
-                ans.Append(first[i]);
-            }
+            if (!current.Children.ContainsKey(ch))
+                current.Children[ch] = new TrieNode();
+            current = current.Children[ch];
         }
 
-        return ans.ToString();
+        current.IsLast = true;
+    }
+
+    string StartsWith(TrieNode node,string prefix)
+    {
+        if (node.Children.Count == 1)
+        {
+            if(node.IsLast)
+            {
+                return prefix;
+            }
+            var ch = node.Children.Keys.First();
+            prefix += ch;
+
+            return StartsWith(node.Children[ch], prefix);
+        }
+        return prefix;
     }
 }
