c7f8fd09d04d96a62d94f013eeeb8a1b1b028203
Somit Meshram <somitmeshram3256@gmail.com>
2024-05-20 14:26:37 +0530

    Time: 88 ms (77.34%), Space: 47.4 MB (5.14%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cs
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cs
@@ -1,27 +1,35 @@
 public class Solution {
-    private Dictionary<int, bool> visted = new();
     public bool CanVisitAllRooms(IList<IList<int>> rooms) {
-for(int i=0;i<rooms.Count;i++)
-        {
-            visted[i] = false;
+        //Build adj List
+        Dictionary<int,List<int>> adjList= new Dictionary<int,List<int>>();
+        int n=rooms.Count;
+        for(int i=0;i<n;i++){
+            adjList[i]=new List<int>(rooms[i]);
         }
-        DFS(0, rooms, visted);
-        foreach (bool x in visted.Values)
-        {
-            if (!x) return false;
-        }
-
-        return true;
-    }
-    public void DFS(int source, IList<IList<int>> rooms, Dictionary<int, bool> visted)
-    {
-        visted[source] = true;
-        foreach (var room in rooms[source])
-        {
-            if (!visted[room])
-            {
-                DFS(room, rooms, visted);
+        
+        //BFS
+        
+        Queue<int> queue= new Queue<int>();
+        bool[] visited= new bool[n];
+        queue.Enqueue(0);
+        visited[0]=true;
+        
+        while(queue.Count>0){
+            var currentNode=queue.Dequeue();
+            foreach(var v in adjList[currentNode]){
+                if(!visited[v]){
+                    queue.Enqueue(v);
+                    visited[v]=true;
+                }
             }
         }
+        
+        for(int i=0;i<n;i++)
+            if(!visited[i])
+                return false;
+        
+        return true;
+        
+        
     }
 }
