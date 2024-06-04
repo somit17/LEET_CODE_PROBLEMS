f8a9876c9815f1b5f43f4181fd09548638ca09db
Somit Meshram <somitmeshram3256@gmail.com>
2024-06-04 23:57:01 +0530

    Time: 71 ms (83.83%), Space: 41.7 MB (97.30%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cs
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public int[] CountBits(int n) {
-        int[] counting=new int[n+1];
-        for(int i=0;i<=n;i++){
-            var numInString=Convert.ToString(i,2);
-            int count=0;
-            foreach(char ch in numInString){
-                if(ch=='1')
-                    count++;    
-            }
-            counting[i]=count;
+        if(n==0) return new int[]{0};
+        int[] res=new int[n+1];
+        res[0]=0;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0)
+                res[i]=res[i/2]+1;
+            else
+                res[i]=res[i/2];
         }
-        return counting;
+        return res;
     }
 }
